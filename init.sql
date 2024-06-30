c508b1bfd5c3e25fbb79fbee72411bf4c722145d
DenisGremaud <32323238+DenisGremaud@users.noreply.github.com>
2024-06-30 17:50:41 +0200

    add tables users and session by users

--- init.sql
+++ init.sql
@@ -4,6 +4,19 @@ CREATE TABLE IF NOT EXISTS collections (
     desc_collection TEXT NOT NULL,
     host VARCHAR(255) NOT NULL,
     port INTEGER NOT NULL,
+	search_k INTEGER NOT NULL,
     hash_collection VARCHAR(255) NOT NULL,
     last_update TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    uuid VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS sessions_users (
+    id SERIAL PRIMARY KEY,
+    user_uuid VARCHAR(255) NOT NULL,
+    session_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_uuid) REFERENCES users(uuid)
 );
